14c2fbabcb1e63194d196a55ce3173f0f4d00729
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-10 20:25:27 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2WeeksGameJam_Roguelike
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            asd.Engine.Initialize("Roguqlike", 640, 480, new asd.EngineOption());
+
+            while (asd.Engine.DoEvents())
+            {
+                asd.Engine.Update();
+            }
+            asd.Engine.Terminate();
+        }
+    }
+}
