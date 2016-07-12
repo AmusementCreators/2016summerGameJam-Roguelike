4093917150a2f7a6eaf165ded5e1c2ce5aa6bf6b
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-13 03:02:44 +0900

    fix license notation

--- Resource.cs
+++ Resource.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿/*============================================================================
+  Copyright (C) 2016 akitsu-sanae, ding, lenny-yusei.
+  https://github.com/AmusementCreators/2016summerGameJam-Roguelike
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-/*============================================================================
-  Copyright (C) 2016 akitsu-sanae, ding, lenny-yusei.
-  https://github.com/akitsu-sanae/gureyuu
-  Distributed under the Boost Software License, Version 1.0. (See accompanying
-  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
-============================================================================*/
 
 namespace _2WeeksGameJam_Roguelike
 {
