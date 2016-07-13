8edddbf61261c86771802474bf86f8eaea5802d1
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-13 11:30:16 +0900

    add Player

---
+++ Character/Player.cs
@@ -0,0 +1,38 @@
+﻿/*============================================================================
+  Copyright (C) 2016 akitsu-sanae, ding, lenny-yusei.
+  https://github.com/AmusementCreators/2016summerGameJam-Roguelike
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2WeeksGameJam_Roguelike.Character
+{
+    class Player : asd.TextureObject2D
+    {
+        public Player()
+        {
+            this.Texture = Resource.Image;
+            this.Src = new asd.RectF(0, Consts.Chip.Height, Consts.Chip.Width, Consts.Chip.Height);
+        }
+
+        protected override void OnUpdate()
+        {
+            var diff = new asd.Vector2DF(0, 0);
+            if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Left) == asd.KeyState.Hold)
+                diff.X -= 2;
+            if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Right) == asd.KeyState.Hold)
+                diff.X += 2;
+            if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Up) == asd.KeyState.Hold)
+                diff.Y -= 2;
+            if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Down) == asd.KeyState.Hold)
+                diff.Y += 2;
+            this.Position += diff;
+        }
+    }
+}
--- Scene/Game.cs
+++ Scene/Game.cs
@@ -19,6 +19,8 @@ namespace _2WeeksGameJam_Roguelike.Scene
         {
             var layer = new asd.Layer2D();
 
+            layer.AddObject(new Character.Player());
+
             layer.AddObject(field);
             AddLayer(layer);
         }
