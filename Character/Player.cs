657146d8df79928da128a3dbf21a65e71b6d628f
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-14 01:53:25 +0900

    fix player moving

--- Character/Player.cs
+++ Character/Player.cs
@@ -17,23 +17,39 @@ namespace _2WeeksGameJam_Roguelike.Character
     {
         public Player()
         {
-            this.Position = new asd.Vector2DF(320, 320);
+            this.Position = new asd.Vector2DF(Consts.Chip.Width*30, Consts.Chip.Height*30);
             this.Texture = Resource.Image;
             this.Src = new asd.RectF(0, Consts.Chip.Height, Consts.Chip.Width, Consts.Chip.Height);
         }
 
         protected override void OnUpdate()
         {
-            var diff = new asd.Vector2DF(0, 0);
+            if (step != 0)
+            {
+                this.Position += speed;
+                step--;
+                return;
+            }
+
+            var diff = new asd.Vector2DF();
             if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Left) == asd.KeyState.Hold)
-                diff.X -= 2;
+                diff.X -= Consts.Chip.Width / MaxStep;
             if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Right) == asd.KeyState.Hold)
-                diff.X += 2;
+                diff.X += Consts.Chip.Width / MaxStep;
             if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Up) == asd.KeyState.Hold)
-                diff.Y -= 2;
+                diff.Y -= Consts.Chip.Height / MaxStep;
             if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Down) == asd.KeyState.Hold)
-                diff.Y += 2;
-            this.Position += diff;
+                diff.Y += Consts.Chip.Height / MaxStep;
+
+            if (diff != new asd.Vector2DF())
+            {
+                this.speed = diff;
+                step = MaxStep;
+            }
         }
+
+        private const int MaxStep = 8;
+        private int step = 0;
+        private asd.Vector2DF speed = new asd.Vector2DF();
     }
 }
