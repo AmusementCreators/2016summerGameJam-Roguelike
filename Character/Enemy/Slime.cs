4f279dde016286681a6f1122088f1f76f4dd6075
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-15 11:54:04 +0900

    add enemy slime

---
+++ Character/Enemy/Slime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2WeeksGameJam_Roguelike.Character.Enemy
+{
+    class Slime : asd.TextureObject2D
+    {
+        public Slime()
+        {
+            this.Texture = Resource.Image;
+            this.Src = new asd.RectF(Consts.Chip.Width, Consts.Chip.Height, Consts.Chip.Width, Consts.Chip.Height);
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
