0a035db62d0e5b9706124ae21d418c3425c9eb8b
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-20 23:57:12 +0900

    add license notation

--- Turn/Enemy.cs
+++ Turn/Enemy.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*============================================================================
+  Copyright (C) 2016 akitsu-sanae, ding, lenny-yusei.
+  https://github.com/AmusementCreators/2016summerGameJam-Roguelike
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
