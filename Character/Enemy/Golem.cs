61d28319fcd4431443776675782d76657ffb15fe
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-25 16:02:24 +0900

    golem walk

--- Character/Enemy/Golem.cs
+++ Character/Enemy/Golem.cs
@@ -31,19 +31,39 @@ namespace _2WeeksGameJam_Roguelike.Character.Enemy
 
         protected override asd.Vector2DF Move()
         {
-            switch (Resource.Rand.Next(0, 3))
+            var diff = charactorSet.player.Position - Position;
+            if (diff.Length > 100) // プレイヤーから遠ければランダムウォーク
             {
-                case 0:
-                    return new asd.Vector2DF(-Consts.Chip.Width, 0);
-                case 1:
-                    return new asd.Vector2DF(Consts.Chip.Width, 0);
-                case 2:
-                    return new asd.Vector2DF(0, -Consts.Chip.Height);
-                case 3:
-                    return new asd.Vector2DF(0, Consts.Chip.Height);
-                default:
-                    return new asd.Vector2DF();
+                switch (Resource.Rand.Next(0, 3))
+                {
+                    case 0:
+                        return new asd.Vector2DF(-Consts.Chip.Width, 0);
+                    case 1:
+                        return new asd.Vector2DF(Consts.Chip.Width, 0);
+                    case 2:
+                        return new asd.Vector2DF(0, -Consts.Chip.Height);
+                    case 3:
+                        return new asd.Vector2DF(0, Consts.Chip.Height);
+                    default:
+                        return new asd.Vector2DF();
+                }
             }
+            else // プレイヤーと近ければ近付いてくる
+            {
+                var result = new asd.Vector2DF();
+                var angle = (diff.Degree + 360)%360 - 45;
+                if (angle < 90)
+                    result.Y = Consts.Chip.Height;
+                else if (angle < 180)
+                    result.X = -Consts.Chip.Width;
+                else if (angle < 270)
+                    result.Y = -Consts.Chip.Height;
+                else
+                    result.X = Consts.Chip.Width;
+
+                return result;
+            }
+
         }
     }
 }
