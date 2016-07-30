0a2373c15f653cfc07649aa3dc3eea9d732b29e4
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-30 23:40:48 +0900

    add title background

--- Scene/Title.cs
+++ Scene/Title.cs
@@ -18,6 +18,10 @@ namespace _2WeeksGameJam_Roguelike.Scene
         public Title()
         {
             var layer = new asd.Layer2D();
+            var background = new asd.TextureObject2D();
+            background.Texture = asd.Engine.Graphics.CreateTexture2D("Resource/title.png");
+            background.Scale = new asd.Vector2DF(4.0f, 4.0f);
+            layer.AddObject(background);
 
             var label = new asd.TextObject2D();
             label.Font = Resource.BigFont;
